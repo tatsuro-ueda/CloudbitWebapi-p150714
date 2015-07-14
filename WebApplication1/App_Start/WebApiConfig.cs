1bfb7bcf02b615433d593173c282c5e0d80e968d
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-07-14 10:11:29 +0900

    ➕JSON出力

--- WebApplication1/App_Start/WebApiConfig.cs
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace WebApplication1
 {
@@ -19,6 +20,8 @@ namespace WebApplication1
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
